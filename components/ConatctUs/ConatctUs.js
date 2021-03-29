18d524fe93d9e4867b16e6cee9a5b4a9e7360a27
ahmed <abouweb1@gmail.com>
2021-03-29 18:58:07 +0200

    integrate the "contact us" form with the back end API

--- components/ConatctUs/ConatctUs.js
+++ components/ConatctUs/ConatctUs.js
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useRouter } from "next/router";
+import requester from "../../utilities/requester";
 import PrimaryButton from "../Button/PrimaryButton";
 import styles from "./ContactUs.module.scss"
 import Lottie from 'react-lottie';
@@ -14,22 +15,59 @@ const defaultOptions = {
     }
 };
 
+const initialFormValues = {
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: "",
+};
+
 const ConatctUs = () => {
     const router = useRouter();
-    const [form, setForm] = useState({
-        name: "",
-        email: "",
-        phone: "",
-        subject: "",
-        message: ""
-    })
+    const [form, setForm] = useState(initialFormValues)
+    const [error, setError] = useState(null)
 
     const inputChangeHandler = (e) => {
-        console.log(e.target.value)
+        // console.log(e.target.value)
         let newFormData = { ...form };
         newFormData[e.target.name] = e.target.value;
         setForm({ ...newFormData });
     }
+
+
+    const validateEmailOrPhone =(email, phone)=> {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const isValidEmail = re.test(String(email).toLowerCase());
+        const isValidPhone = phone.length >= 7;
+        console.log(isValidEmail, isValidPhone)
+        return (isValidEmail||isValidPhone)
+
+    }
+
+
+    const formValidator =(formdata)=>{
+
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        setError(null)
+        if(validateEmailOrPhone(form.email, form.phone)){
+            let payload = {...form, time : Date.now().toString()};
+            // console.log(payload);
+            requester.post("/messages/sendMessage", payload).then(()=>{
+                window.alert(router.locale === "ar" ? "تم إرسال الرساللة بنجاح"  : "Message Sent Successfully")
+            }).catch(()=>{
+                window.alert("Error Occurred");
+            })
+            setForm(initialFormValues);
+        }
+        else {
+            setError("invalid Email or Phone")
+        }
+    }
+
     return (
         <section id={"#contactUs"} className={styles.ConatctUs}>
             <div className="container" dir="auto">
@@ -43,21 +81,23 @@ const ConatctUs = () => {
                             width={"100%"}
                         />
                     </div>
-                    <form className={styles.form}>
+                    <form className={styles.form} onSubmit={handleSubmit}>
                         <div className={styles.formSharedRow}>
-                            <input type="text" name="name" required placeholder={router.locale === "ar" ? "إسمك" : "Your Name"} value={form.name} onChange={inputChangeHandler} />
+                            <input type="text" name="name" required={true} placeholder={router.locale === "ar" ? "إسمك" : "Your Name"} value={form.name} onChange={inputChangeHandler} />
                             <input type="text" name="email" placeholder={router.locale === "ar" ? "بريدك الإلكترونى" : "Your Email"} value={form.email} onChange={inputChangeHandler} />
                         </div>
                         <div className={styles.formRow}>
-                            <input type="text" name="phone" placeholder={router.locale === "ar" ? "رقم هاتفك" : "Your Phone Number"} value={form.phone} onChange={inputChangeHandler} />
+                            <input type="number" name="phone" placeholder={router.locale === "ar" ? "رقم هاتفك" : "Your Phone Number"} value={form.phone} onChange={inputChangeHandler} />
                         </div>
                         <div className={styles.formRow}>
                             <input type="text" name="subject" placeholder={router.locale === "ar" ? "عنوان رسالتك" : "Your Subject"} value={form.subject} onChange={inputChangeHandler} />
                         </div>
                         <div className={styles.formRow}>
-                            <textarea name="message" required value={form.message} placeholder={router.locale === "ar" ? "رسالتك" : "Your Message"} onChange={inputChangeHandler} />
+                            <textarea name="message" required={true} value={form.message} placeholder={router.locale === "ar" ? "رسالتك" : "Your Message"} onChange={inputChangeHandler} />
                         </div>
                         <PrimaryButton>{router.locale === "ar" ? "إرسال" : "Send"}</PrimaryButton>
+                        <br/>
+                        {error&&<span className={styles.error}> {error}</span>}
                     </form>
 
                 </div>
