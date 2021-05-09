4d2a314732f984321de43699e8a26ac93047d0ee
ahmed <hussain>
2021-05-09 03:31:54 +0200

    keyword and google site verfication

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,9 @@ function MyApp({ Component, pageProps }) {
     <DisplayLoadingOverlayHandler.Provider value={setDisplayLoadingOverlay}>
       <div className={"layout"}>
         <Head>
-          <meta name="google-site-verification" content="ONJ1unNt8ctFuKpJ6fV_fJa7f2z1Dog5GStLWWvf9PE" />
+          <meta name="google-site-verification" content="udoFOBrAzGjO_24zy55qfVL-UsWhps-VhuPSEV4AIW0" />
+          <meta name="keywords" content="لصق, شمع, لصق إنشائى, لصق دوكو, شمع اصفر, مسامير, بويه, مزيل صدئ, سنفرة, لصق بنائي, شحم, ملمع معادن, حديد, حدايد خشب, نجارة, اوكر, اصابع شمع, مسدس شمع, ابواب, ابو, ابو العزم, طارق ابو العزم, انشائى, تشطيب, بناء, تشييد, دهان, صنفرة, سمكرة, خردوات"/>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
           <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
           <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
           <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
